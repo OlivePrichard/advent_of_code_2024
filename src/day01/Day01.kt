201108ad749f2abb7a0f838b02e3aae4c3bf9f93
Olive Prichard <70983271+ChillyCoyote273@users.noreply.github.com>
2024-12-01 00:29:46 -0500

    day 1

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -2,20 +2,31 @@ package day01
 
 import println
 import readInput
+import kotlin.math.abs
 
 fun part1(input: List<String>): Int {
-    return input.size
+    val locations = input.map { it.split("   ") }
+    val firstLocations = locations.map { it.first().toInt() }.sorted()
+    val secondLocations = locations.map { it.last().toInt() }.sorted()
+    return firstLocations.zip(secondLocations).sumOf { abs(it.first - it.second) }
 }
 
 fun part2(input: List<String>): Int {
-    return input.size
+    val locations = input.map { it.split("   ") }
+    val firstLocations = locations.map { it.first().toInt() }.sorted()
+    val secondLocations = locations.map { it.last().toInt() }.sorted()
+    var similarity = 0
+    for (firstLocation in firstLocations) {
+        similarity += firstLocation * secondLocations.count { it == firstLocation }
+    }
+    return similarity
 }
 
 fun main() {
     // Read a test input from the `src/day01/Day01_test.txt` file:
     val testInput = readInput("day01/Day01_test")
-    check(part1(testInput) == testInput.size)
-    check(part2(testInput) == testInput.size)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/day01/Day01.txt` file:
     val input = readInput("day01/Day01")
