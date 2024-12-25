b5859ac1ce8d24172a74c657da964179d4ce3439
Olive Prichard <70983271+ChillyCoyote273@users.noreply.github.com>
2024-12-25 00:10:55 -0500

    day 25

--- src/day25/Day25.kt
+++ src/day25/Day25.kt
@@ -4,21 +4,39 @@ import println
 import readInput
 
 fun part1(input: List<String>): Int {
-    return input.size
-}
-
-fun part2(input: List<String>): Int {
-    return input.size
+    val schematics = input.joinToString("\n").split("\n\n").map { line ->
+        line.split("\n").map { it.toCharArray().toList() }
+    }
+    val locks = mutableListOf<List<List<Char>>>()
+    val keys = mutableListOf<List<List<Char>>>()
+    for (schematic in schematics) {
+        if (schematic[0][0] == '.')
+            keys.add(schematic)
+        else
+            locks.add(schematic)
+    }
+    var count = 0
+    for (key in keys) {
+        pair@for (lock in locks) {
+            for (row in lock.indices) {
+                for (col in lock[row].indices) {
+                    if (lock[row][col] == '#' && key[row][col] == '#') {
+                        continue@pair
+                    }
+                }
+            }
+            count++
+        }
+    }
+    return count
 }
 
 fun main() {
     // Read a test input from the `src/day25/Day25_test.txt` file:
     val testInput = readInput("day25/Day25_test")
-    check(part1(testInput) == testInput.size)
-    check(part2(testInput) == testInput.size)
+    check(part1(testInput) == 3)
 
     // Read the input from the `src/day25/Day25.txt` file:
     val input = readInput("day25/Day25")
     part1(input).println()
-    part2(input).println()
 }
